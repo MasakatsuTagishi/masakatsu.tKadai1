17a6292f0d32f075a9f580edd761d2121429531a
MasakatsuTagishi <m.t-10.22@docomo.ne.jp>
2022-01-19 21:29:54 +0900

    課題提出

--- Kadai1/ViewController.swift
+++ Kadai1/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var textField3: UITextField!
+    @IBOutlet private weak var textFieid4: UITextField!
+    @IBOutlet private weak var textField5: UITextField!
+    @IBOutlet private weak var totalLabel: UILabel!
+    
+    @IBAction private func pressButton(_ sender: UIButton) {
+        totalLabel.text = String(calculate())
     }
+    
+    private func calculate() -> Int {
+        let num1 = textField1.textToInt
+        let num2 = textField2.textToInt
+        let num3 = textField3.textToInt
+        let num4 = textFieid4.textToInt
+        let num5 = textField5.textToInt
+        return num1 + num2 + num3 + num4 + num5
+    }
+}
 
-
+extension UITextField {
+    var textToInt: Int {
+        let text = self.text
+        let int = text
+            .flatMap{Int($0)} ?? 0
+        return int
+    }
 }
 
